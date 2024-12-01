987dddd9d51d86d5c29e82c69a781f23700a89a3
hunterleeann <hunterleeann41210@gmail.com>
2024-11-30 16:08:44 -0800

    console.log change

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,36 @@ import { puppyList } from "./data";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  const [featPupId, setFeatPupId] = useState()
-  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState();
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  // console.log(puppies);
   console.log(featuredPup);
 
-  {featPupId && (
-    <div>
-      <h2>{featuredPup.name}</h2>
-      <ul>
-        <li>Age: {featuredPup.age}</li>
-        <li>Email: {featuredPup.email}</li>
-      </ul>
-    </div>
-  )}
-  
+  {
+    featPupId && (
+      <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>
+    );
+  }
+
   return (
     <>
       {puppies.map((puppy) => {
-        return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
     </>
   );
